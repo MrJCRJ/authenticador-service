2f9c231eee81e7291504cf5579e4874ba737231c
Jose Cicero <mr.jociriju@gmail.com>
2025-03-13 18:36:17 -0300

    Adicionar comentários explicativos para melhorar a legibilidade do módulo principal da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 // src/app.module.ts
 
+// Importa o decorador `Module` do pacote @nestjs/common, que é usado para definir um módulo no NestJS.
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller'; // Importe o controlador
+
+// Importa o controlador principal da aplicação.
+import { AppController } from './app.controller';
+
+// Importa o módulo de autenticação, que contém controladores, provedores e outros componentes relacionados à autenticação.
 import { AuthModule } from './auth/auth.module';
 
+// Define o módulo principal da aplicação usando o decorador @Module.
 @Module({
+  // A lista de módulos importados. Neste caso, estamos importando o AuthModule,
+  // que pode conter funcionalidades relacionadas à autenticação e autorização.
   imports: [AuthModule],
-  controllers: [AppController], // Adicione o controlador à lista de controladores
+
+  // A lista de controladores que pertencem a este módulo.
+  // O AppController é o controlador principal da aplicação, que lida com as rotas raiz.
+  controllers: [AppController],
+
+  // A lista de provedores (services, repositories, etc.) que pertencem a este módulo.
+  // Neste caso, não há provedores definidos no módulo principal.
   providers: [],
 })
+// Exporta a classe AppModule, que representa o módulo principal da aplicação.
 export class AppModule {}
